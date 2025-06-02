8f10b4254d9b885ea5f589247d173fc6d9234899
Ramahabir <azzurro2584@gmail.com>
2025-06-02 19:15:02 +0700

    new file:   SCH_Robot-Transporter_2025-06-01.json 	modified:   main/main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,33 @@
-// Motor A (belakang kiri)
-const int motorPin1  = 5;  
-const int motorPin2  = 6;  
-// Motor B (belakang kanan)  
-const int motorPin3  = 10; 
-const int motorPin4  = 9;  
+#include <Servo.h>
+
+// Motor A (Back Left)
+
+#define motorPin1 5  // Pin 14 of L293
+#define motorPin2 6  // Pin 10 of L293
+// Motor B (Back Right)  
+#define motorPin3 10 // Pin  7 of L293
+#define motorPin4 9  // Pin  2 of L293
 
 // Front Motors (L293D #2)
-// Motor C (depan kiri)
-const int motorPin5  = 3;  
-const int motorPin6  = 2;  
-// Motor D (depan kanan)
-const int motorPin7  = 11; 
-const int motorPin8  = 12; 
+// Motor C (Front Left)
+#define motorPin5 3  // Pin 14 of L293
+#define motorPin6 2 // Pin 10 of L293
+// Motor D (Front Right)
+#define motorPin7 11 // Pin  7 of L293
+#define motorPin8 12 // Pin  2 of L293
+
+// Servo 1 A0
+// Servo 2 A1
+
+Servo myservo1;
+Servo myservo2;
+
+String cmd;
+
+
 
 void setup() {
+  //Motor
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorPin3, OUTPUT);
@@ -23,18 +37,48 @@ void setup() {
   pinMode(motorPin7, OUTPUT);
   pinMode(motorPin8, OUTPUT);
   
+  //Servo
+  myservo1.attach(A0);
+  myservo2.attach(A1);
+  myservo1.write(0);
+  myservo2.write(0);
+
+
+
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  kekiri();
 
+  if (Serial.available()){
+  
+    String cmd = Serial.readString();
+    if (cmd == "Maju"){
+    	maju();
+    } else if (cmd == "Mundur"){
+    	mundur();
+    } else if (cmd == "Kiri"){
+    	kekiri();
+    } else if (cmd == "Kanan"){
+    	kekanan();
+    } else if (cmd == "Stop"){
+    	berhenti();
+    } else if (cmd == "servo1"){
+    	servo1_putar();
+    } else if(cmd == "servo2"){
+    	servo2_putar();
+    } else if(cmd == "servo1_balik"){
+    	servo1_putarbalik();
+    } else if(cmd == "servo1_balik"){
+    	servo2_putarbalik();
+    }
+  }
 }
 
 
 void maju() {
   Serial.println("Maju");
-
   digitalWrite(motorPin1, HIGH);  // maju
   digitalWrite(motorPin2, LOW);
   digitalWrite(motorPin3, HIGH);  // maju
@@ -99,4 +143,28 @@ void berhenti() {
   digitalWrite(motorPin6, LOW);
   digitalWrite(motorPin7, LOW);
   digitalWrite(motorPin8, LOW);
+}
+
+void servo1_putar(){
+  for (int i =0; i <=180; i++){
+  	myservo1.write(i);
+  }
+}
+
+void servo1_putarbalik(){
+  for (int i =180; i >= 0; i--){
+  	myservo1.write(i);
+  }
+}
+
+void servo2_putar(){
+  for (int i =0; i <=180; i++){
+  	myservo2.write(i);
+  }
+}
+
+void servo2_putarbalik(){
+  for (int i =180; i >= 0; i--){
+  	myservo2.write(i);
+  }
 }
